2cf6d58cc8588706b8a470ca9229106305e1bef5
Douglas Squirrel <squirrel@blindpages.com>
2009-08-23 02:18:34 +0000

    Tidy pages; programmed detail

--- app/controllers/unlock_controller.rb
+++ app/controllers/unlock_controller.rb
@@ -4,9 +4,9 @@ require 'open-uri'
 
 class UnlockController < ApplicationController
   def show
-    page = Page.find_by_path(request.path)
-    if (page.nil?) then render(:text => "Page #{request.path} doesn't exist.")
-    else                @content = eval(page.code)
+    @page = Page.find_by_path(request.path)
+    if (@page.nil?) then render(:text => "Page #{request.path} doesn't exist.")
+    else                @content = eval(@page.code)
     end
   end
 end
