b6dad8ccb31f71edc50c97a081fa48c1fabfa745
Douglas Squirrel <squirrel@blindpages.com>
2009-08-29 13:31:52 +0000

    Render simple xml

--- app/controllers/unlock_controller.rb
+++ app/controllers/unlock_controller.rb
@@ -7,7 +7,11 @@ class UnlockController < ApplicationController
   def show
     @page = Page.find_by_path(request.path)
     if (@page.nil?) then render(:text => "Page #{request.path} doesn't exist.")
-    else                @content = eval(@page.code)
+    else                 @content = eval(@page.code)
+    end
+    respond_to do |format|
+      format.html # unlock.html.erb
+      format.xml  { render :xml => "<hello>world</hello>" }
     end
   end
 end
