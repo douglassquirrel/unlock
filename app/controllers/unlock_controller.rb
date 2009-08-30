15e9ae70fa89b67a2d17b89b74476fe67425244f
Douglas Squirrel <squirrel@blindpages.com>
2009-08-30 00:13:10 +0000

    Go to previous and next choices

--- app/controllers/unlock_controller.rb
+++ app/controllers/unlock_controller.rb
@@ -20,12 +20,19 @@ class UnlockController < ApplicationController
 
   def convert_to_voice_xml
     doc = Nokogiri::HTML(@content)
-    choices = []
-    doc.xpath("//ul//a").enum_with_index.each { |node, number| 
-        break if number>=8; 
-        choices << { :number => number, :url => node.xpath("@href").to_s + "&format=xml", :prompt => node.xpath("text()") } 
-    }
+    if params[:choice_start] then choice_start = params[:choice_start].to_i; url = request.url
+    else                          choice_start = 0;                          url = add_parameter(request.url, "choice_start=0")
+    end
+    choices = doc.xpath("//ul//a").collect { |node| { :url => add_parameter(node.xpath("@href").to_s, "format=xml"), :prompt => node.xpath("text()") } }
+    total_choices = choices.size
+    choices = (choices[choice_start, 7] || []).enum_with_index.each { |choice, number| choice[:number] = number }
+    if choice_start > 0                 then choices << { :number => 8, :url => url.sub(/choice_start=\d*/, "choice_start=#{[0, choice_start-7].max}"), :prompt => "Previous choices" } end
+    if choice_start < total_choices - 7 then choices << { :number => 9, :url => url.sub(/choice_start=\d*/, "choice_start=#{choice_start+7}"),          :prompt => "More choices" } end
     @content = { :paragraphs => doc.xpath("//p/text()").collect { |node| node.to_s },
                  :choices    => choices }
   end 
+
+  def add_parameter(url, parameter)
+    url.include?("?") ? "#{url}&#{parameter}" : "#{url}?#{parameter}"
+  end
 end
