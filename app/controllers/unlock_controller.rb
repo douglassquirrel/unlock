1275fc0d9bf0b6f88c413093a2daac071eacd8d6
Douglas Squirrel <squirrel@blindpages.com>
2009-08-30 01:12:08 +0000

    Add star for back

--- app/controllers/unlock_controller.rb
+++ app/controllers/unlock_controller.rb
@@ -23,11 +23,12 @@ class UnlockController < ApplicationController
     if params[:choice_start] then choice_start = params[:choice_start].to_i; url = request.url
     else                          choice_start = 0;                          url = add_parameter(request.url, "choice_start=0")
     end
-    choices = doc.xpath("//ul//a").collect { |node| { :url => add_parameter(node.xpath("@href").to_s, "format=xml"), :prompt => node.xpath("text()") } }
+    choices = doc.xpath("//ul//a[not(@id='back')]").collect { |node| { :url => add_parameter(node.xpath("@href").to_s, "format=xml"), :prompt => node.xpath("text()") } }
     total_choices = choices.size
     choices = (choices[choice_start, 7] || []).enum_with_index.each { |choice, number| choice[:number] = number }
     if choice_start > 0                 then choices << { :number => 8, :url => url.sub(/choice_start=\d*/, "choice_start=#{[0, choice_start-7].max}"), :prompt => "Previous choices" } end
     if choice_start < total_choices - 7 then choices << { :number => 9, :url => url.sub(/choice_start=\d*/, "choice_start=#{choice_start+7}"),          :prompt => "More choices" } end
+    back = doc.xpath("//ul//a[@id='back']")[0]; if back then choices << { :number => 'star', :url => add_parameter(back.xpath("@href").to_s, "format=xml"), :prompt => back.xpath("text()") } end
     @content = { :paragraphs => doc.xpath("//p/text()").collect { |node| node.to_s },
                  :choices    => choices }
   end 
