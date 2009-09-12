01f32c575ab196bb3930599efc932551a8658371
Douglas Squirrel <squirrel@blindpages.com>
2009-09-12 22:38:12 +0000

    Fix logout action; log connid on login and logout

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,16 +9,21 @@ class AuthController < ApplicationController
     if (@user.nil?) then 
       log "Failed login"
       flash[:notice] = "Passcode not recognised"
-      redirect_to :action => "index", :format => params[:format]
+      redirect_to :action => "index", :format => params[:format], :connid => params[:connid]
     else
       log "Successful login by user #{@user.first_name} #{@user.last_name}"   
       redirect_to "/home?user=#{@user.first_name}&format=#{params[:format]}"
     end
   end
 
+  def logout
+    log "Logout"
+    render :nothing => true
+  end
+
   private
   def log(text)
-    logger.info "BLINDPAGES: #{Time.new.to_s} #{text}"
+    logger.info "BLINDPAGES: #{Time.new.to_s} #{params[:connid]} #{text}"
   end
 end
 
