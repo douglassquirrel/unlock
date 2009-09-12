bfdd3855225a104c9183ebf1fc0064d364728bac
Douglas Squirrel <squirrel@blindpages.com>
2009-09-12 20:03:59 +0000

    Passcode on phone

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -8,7 +8,7 @@ class AuthController < ApplicationController
       flash[:notice] = "Passcode not recognised"
       redirect_to :action => "index"
     else
-      redirect_to "/home?user=#{@user.first_name}"
+      redirect_to "/home?user=#{@user.first_name}&format=#{params[:format]}"
     end
   end
 end
