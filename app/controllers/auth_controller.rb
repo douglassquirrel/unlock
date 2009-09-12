8e61cbe877a24475872cb319b612948fc9e8b2c9
Douglas Squirrel <squirrel@blindpages.com>
2009-09-12 20:50:02 +0000

    Phone version of login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
     @user = User.find_by_passcode(params[:passcode])
     if (@user.nil?) then 
       flash[:notice] = "Passcode not recognised"
-      redirect_to :action => "index"
+      redirect_to :action => "index", :format => params[:format]
     else
       redirect_to "/home?user=#{@user.first_name}&format=#{params[:format]}"
     end
